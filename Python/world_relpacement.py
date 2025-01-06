ccb25ad69a242ccc60ca7cb976e3e840607e0438
Dhananjay Gangwar <kannu.2097@gmail.com>
2025-01-06 13:11:53 +0530

    test

---
+++ Python/world_relpacement.py
@@ -0,0 +1,12 @@
+
+def replace_word():
+
+    str = "Hi guys, i am djay, and hi hi hi hi"
+    word_to_replace = input("enter the word to replace: ")
+    word_replacement = input("Enter the replacement word ")
+    print(str.replace(word_to_replace,word_replacement))
+
+replace_word()
+
+
+
